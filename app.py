4e25fead7dbf8b9851aece22443c439cae11b424
amanrao23 <amanrao18613@gmail.com>
2020-04-30 01:15:48 +0530

    added disaster home page

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import PrimaryKeyConstraint, ForeignKeyConstraint
 
-from forms import Newctrlcntrform, Loginctrlform,Newngoform,Loginngoform
+from forms import Newctrlcntrform, Loginctrlform, Newngoform, Loginngoform
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from functools import wraps
@@ -20,31 +20,32 @@ login_manager.login_view = 'logincontrol'
 
 
 class Allusers(UserMixin, db.Model):  # all users login id and password are stored here
-    id=db.Column(db.Integer,primary_key=True)
-    username = db.Column(db.String(20), unique=True,nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
     role = db.Column(db.String(80), default='guest')
 
 
-class Control(db.Model):  #control centre class
-    id=db.Column(db.Integer,primary_key=True)  #disaster id
+class Control(db.Model):  # control centre class
+    id = db.Column(db.Integer, primary_key=True)  # disaster id
     c_name = db.Column(db.String(20))
-    c_username = db.Column(db.String(20),db.ForeignKey('allusers.username'),nullable=False)
+    c_username = db.Column(db.String(20), db.ForeignKey('allusers.username'), nullable=False)
     c_password = db.Column(db.String(60), nullable=False)
     ngo_manage = db.relationship('Ngo', backref='Control')
 
 
 class Ngo(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     ngo_name = db.Column(db.String(20))
-    control_id = db.Column(db.Integer, db.ForeignKey('control.id'), nullable=False)#disaster id
-    ngo_username = db.Column(db.String(20),db.ForeignKey('allusers.username'),nullable=False)
+    control_id = db.Column(db.Integer, db.ForeignKey('control.id'), nullable=False)  # disaster id
+    ngo_username = db.Column(db.String(20), db.ForeignKey('allusers.username'), nullable=False)
     ngo_password = db.Column(db.String(60), nullable=False)
+
 
 class Offsitehelper(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
-    username = db.Column(db.String(20), db.ForeignKey('allusers.username'),nullable=False)
+    username = db.Column(db.String(20), db.ForeignKey('allusers.username'), nullable=False)
     password = db.Column(db.String(60), nullable=False)
 
 
@@ -61,7 +62,8 @@ class Segregatedtweet(db.Model):
     location = db.Column(db.String(200), nullable=True)
     status = db.Column(db.String(20), nullable=True)  # assigned/verified/matched etc
     type = db.Column(db.String(20), nullable=False)  # need or availability
-    ngo_id=db.Column(db.Integer,db.ForeignKey('ngo.id'))
+    ngo_id = db.Column(db.Integer, db.ForeignKey('ngo.id'))
+
 
 class matchedtweet(db.Model):
     __tablename__ = 'matchedtweet'
@@ -77,9 +79,9 @@ class matchedtweet(db.Model):
 
 
 class Comments(db.Model):
-    username = db.Column(db.String(20),db.ForeignKey('allusers.username'),primary_key=True, nullable=False)
-    comment=db.Column(db.String(200))
-    tweetid=db.Column(db.Integer,db.ForeignKey('segregatedtweet.id'),nullable=False)
+    username = db.Column(db.String(20), db.ForeignKey('allusers.username'), primary_key=True, nullable=False)
+    comment = db.Column(db.String(200))
+    tweetid = db.Column(db.Integer, db.ForeignKey('segregatedtweet.id'), nullable=False)
 
 
 @login_manager.user_loader
@@ -99,6 +101,7 @@ def login_required(role="admin"):
 
     return wrapper
 
+
 def login_required(role="ngo"):
     def wrapper(fn):
         @wraps(fn)
@@ -110,6 +113,7 @@ def login_required(role="ngo"):
         return decorated_view
 
     return wrapper
+
 
 def login_required(role="ngo"):
     def wrapper(fn):
@@ -124,10 +128,7 @@ def login_required(role="ngo"):
     return wrapper
 
 
-
-
-
-@app.route('/registerctrlcntr', methods=['GET', 'POST']) #registration of a control centre
+@app.route('/registerctrlcntr', methods=['GET', 'POST'])  # registration of a control centre
 def registerctrlcntr():
     form = Newctrlcntrform()
     if form.validate_on_submit():
@@ -140,13 +141,13 @@ def registerctrlcntr():
     return render_template('registercntlcntr.html', form=form)
 
 
-@app.route('/logincontrol', methods=['GET', 'POST']) #login of a control centre
+@app.route('/logincontrol', methods=['GET', 'POST'])  # login of a control centre
 def logincontrol():
     form = Loginctrlform()
     if form.validate_on_submit():
         # return form.username.data + ' ' + form.password.data
         user = Allusers.query.filter_by(username=form.username.data).first()
-        controluser=Control.query.filter_by(c_username=form.username.data).first()
+        controluser = Control.query.filter_by(c_username=form.username.data).first()
         if user and controluser:
             if (user.password == form.password.data):
                 login_user(user, remember=form.remember.data)
@@ -161,20 +162,22 @@ def registerngo():
     form = Newngoform()
     if form.validate_on_submit():
         newid = Allusers(username=form.username.data, password=form.password.data, role='ngo')
-        new_ngo = Ngo(ngo_name=form.name.data, control_id=form.disasterId.data  ,ngo_username=form.username.data, ngo_password=form.password.data)
+        new_ngo = Ngo(ngo_name=form.name.data, control_id=form.disasterId.data, ngo_username=form.username.data,
+                      ngo_password=form.password.data)
         db.session.add(new_ngo)
         db.session.add(newid)
         db.session.commit()
         return redirect(url_for('loginngo'))
     return render_template('registerngo.html', form=form)
 
-@app.route('/loginngo', methods=['GET', 'POST']) #login of a control centre
+
+@app.route('/loginngo', methods=['GET', 'POST'])  # login of a control centre
 def loginngo():
     form = Loginngoform()
     if form.validate_on_submit():
         # return form.username.data + ' ' + form.password.data
         user = Allusers.query.filter_by(username=form.username.data).first()
-        ngouser=Ngo.query.filter_by(ngo_username=form.username.data).first()
+        ngouser = Ngo.query.filter_by(ngo_username=form.username.data).first()
         if user and ngouser:
             if (user.password == form.password.data):
                 login_user(user, remember=form.remember.data)
@@ -183,24 +186,35 @@ def loginngo():
         # return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
     return render_template('loginngo.html', form=form)
 
+
+@app.route("/disaster/<int:c_id>")
+def disaster(c_id):
+    disasters = Control.query.get_or_404(c_id)
+    tweets=Segregatedtweet.query.filter(Segregatedtweet.control_id==c_id)
+    return render_template('disasterhome.html', disaster=disasters,tweet=tweets)
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/ngopage')
 @login_required(role="ngo")
 def ngopage():
     return render_template('ngopage.html', name=current_user.username)
 
+
 @app.route('/dashboard')
 @login_required(role="admin")
 def dashboard():
     return render_template('dashboard.html', name=current_user.username)
 
+
 @app.route('/dashboard1')
 @login_required(role="ngo")
 def dashboard1():
     return render_template('dashboard1.html', name=current_user.username)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
